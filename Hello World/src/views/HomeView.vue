ec0722692910c1d3a315abe7fe951eabef4b81a4
Ilkaan <ilkbi@yahoo.de>
2023-05-30 12:23:43 +0200

    frontend 1. Vorlage

--- Hello World/src/views/HomeView.vue
+++ Hello World/src/views/HomeView.vue
@@ -1,6 +1,31 @@
-<script setup>
-//import TheWelcome from '../components/TheWelcome.vue'
+<script>
+
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    getItems() {
+      this.items = []
+      const endpoint = 'http://' + url + '/items/all'
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      }
+      fetch(endpoint, requestOptions)
+          .then((response) => response.json())
+          .then((result) =>
+              result.forEach((thing) => {
+                this.items.push(thing)
+              })
+          )
+          .catch((error) => console.log('error', error))
+    },
+  }}
 </script>
+
 <style>
 p {
   font-size: larger;
@@ -18,7 +43,8 @@ p {
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Navigationbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
@@ -33,25 +59,38 @@ p {
           </div>
         </div>
       </nav>
-      <p>- Event 1 <button style= "margin-right: 70px" type="button" class="btn btn-success">Success</button>
-        <button style="margin-right: 40px" type="button" class="btn btn-danger">Danger</button></p>
-      <p>- Event 2 <button style= "margin-right: 70px" type="button" class="btn btn-success">Success</button>
-        <button type="button" class="btn btn-danger">Danger</button></p>
-      <p>- Event 3 <button style= "margin-right: 70px" type="button" class="btn btn-success">Success</button>
-        <button type="button" class="btn btn-danger">Danger</button></p>
-      <p>- Event 4 <button style= "margin-right: 70px" type="button" class="btn btn-success">Success</button>
-        <button type="button" class="btn btn-danger">Danger</button></p>
-      <p>- Event 5 <button style= "margin-right: 70px" type="button" class="btn btn-success">Success</button>
-        <button type="button" class="btn btn-danger">Danger</button></p>
-      <p>- Event 6 <button style= "margin-right: 70px" type="button" class="btn btn-success">Success</button>
-        <button type="button" class="btn btn-danger">Danger</button></p>
-      <p>- Event 7 <button style= "margin-right: 70px" type="button" class="btn btn-success">Success</button>
-        <button type="button" class="btn btn-danger">Danger</button></p>
-      <p>- Event 8 <button style= "margin-right: 70px" type="button" class="btn btn-success">Success</button>
+      <p>- Event 1
+        <button style="margin-right: 70px" type="button" class="btn btn-success">Success</button>
+        <button style="margin-right: 40px" type="button" class="btn btn-danger">Danger</button>
+      </p>
+      <p>- Event 2
+        <button style="margin-right: 70px" type="button" class="btn btn-success">Success</button>
         <button type="button" class="btn btn-danger">Danger</button>
       </p>
-
-
+      <p>- Event 3
+        <button style="margin-right: 70px" type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+      </p>
+      <p>- Event 4
+        <button style="margin-right: 70px" type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+      </p>
+      <p>- Event 5
+        <button style="margin-right: 70px" type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+      </p>
+      <p>- Event 6
+        <button style="margin-right: 70px" type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+      </p>
+      <p>- Event 7
+        <button style="margin-right: 70px" type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+      </p>
+      <p>- Event 8
+        <button style="margin-right: 70px" type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+      </p>
 
 
     </div>
