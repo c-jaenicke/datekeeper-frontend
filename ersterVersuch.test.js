344d2cf9f114108de16c16d1611a3944d24a6f57
Ilkaan <ilkbi@yahoo.de>
2023-07-06 19:56:29 +0200

    letzter commit

--- ersterVersuch.test.js
+++
@@ -1,7 +0,0 @@
-import ersterVersuch from './ersterVersuch';
-
-test('should know 1+1, ', () => {
-  const expected = 2;
-  const actual = ersterVersuch.add(1, 1);
-  expect(expected).toBe(actual);
-});
